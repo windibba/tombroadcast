4b17385237f2a5b8cc300b9aef18232c49fa306f
windibba <windibba@gmail.com>
2022-07-25 22:56:57 +0800

    third commit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,11 @@
 <script>
 import Navbar from "../lib/Navbar.svelte";
 import '../GlobalCss/global.css' 
+import Footer from "../lib/Footer.svelte";
 
 
 
 </script>
 <Navbar />
 <slot />
+<Footer />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,64 +4,79 @@
         <h3>Welcome to the Party</h3>
     </div>
     
-    <div class="cari">
-        <div class="card">
-            <img src="/card.png" alt="card">
-            <h1>Ngobrolin Masalah Keluarga</h1>
-            <h3>Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Mollitia illum quisquam porro voluptatibus, 
-                voluptatum sint officiis ex!     Atque, dolores! Deserunt.
-            </h3>
-            <div class="comment">
-                <img src="/comment.svg" alt="comment">
-                <h3>Comment</h3>
+    <div class="card-container">
+        <div class="card-item">
+            <div class="image-container">
+                <img src="/card.png" alt="card">
             </div>
-            <div class="like">
-                <img src="/like.svg" alt="like">
-                <h3>Like</h3>
-                <div class="share">
-                    <img src="/share.svg" alt="comment">
-                    <h3>Comment</h3>
+            <div class="content">
+                <div class="content-main">
+                    <h1>Ngobrolin Masalah Keluarga</h1>
+                    <p>lorem ipsum dskjfkldasjfkadsjfkljadsklfjkldsjfkldsjfkladsjfkladsjfkladsjkfljdasklfjkadsljffjkadsfdlkdjsfkladsklfkdsljflkdsjflkdasjfkldasjfkladsjfkl;adsmc,nzcxmnvjhgounfkldsnjadshfioadsjfkljadsl;fjadskljflkadsfkldsajfkladsjfkladslkfhewiofhljadsnfjlsdnm,ncm,xn lcioSF;</p>
+                </div>
+                <div class="content-buttons">
+                    <div class="content-buttons-group">
+                        <img src="/comment.svg" alt="comment">
+                        <p>Comment</p>
+                    </div>
+                    <div class="content-buttons-group">
+                        <img src="/like.svg" alt="like">
+                        <p>Like</p>
+                    </div>
+                    <div class="content-buttons-group">
+                        <img src="/share.svg" alt="share">
+                        <p>Share</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="card">
-            <img src="/card.png" alt="card">
-            <h1>Ngobrolin Masalah Keluarga</h1><br>
-            <h3>Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Mollitia illum quisquam porro voluptatibus, 
-                voluptatum sint officiis ex!     Atque, dolores! Deserunt.
-            </h3>
-            <div class="comment">
-                <img src="/comment.svg" alt="comment">
-                <h3>Comment</h3>
+        <div class="card-item">
+            <div class="image-container">
+                <img src="/card.png" alt="card">
             </div>
-            <div class="like">
-                <img src="/like.svg" alt="like">
-                <h3>Like</h3>
-                <div class="share">
-                    <img src="/share.svg" alt="comment">
-                    <h3>Comment</h3>
+            <div class="content">
+                <div class="content-main">
+                    <h1>Ngobrolin Masalah Keluarga</h1>
+                    <p>lorem ipsum dskjfkldasjfkadsjfkljadsklfjkldsjfkldsjfkladsjfkladsjfkladsjkfljdasklfjkadsljffjkadsfdlkdjsfkladsklfkdsljflkdsjflkdasjfkldasjfkladsjfkl;adsmc,nzcxmnvjhgounfkldsnjadshfioadsjfkljadsl;fjadskljflkadsfkldsajfkladsjfkladslkfhewiofhljadsnfjlsdnm,ncm,xn lcioSF;</p>
+                </div>
+                <div class="content-buttons">
+                    <div class="content-buttons-group">
+                        <img src="/comment.svg" alt="comment">
+                        <p>Comment</p>
+                    </div>
+                    <div class="content-buttons-group">
+                        <img src="/like.svg" alt="like">
+                        <p>Like</p>
+                    </div>
+                    <div class="content-buttons-group">
+                        <img src="/share.svg" alt="share">
+                        <p>Share</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="card">
-            <img src="/card.png" alt="card">
-            <h1>Ngobrolin Masalah Keluarga</h1>
-            <h3>Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Mollitia illum quisquam porro voluptatibus, 
-                voluptatum sint officiis ex!     Atque, dolores! Deserunt.
-            </h3>
-            <div class="comment">
-                <img src="/comment.svg" alt="comment">
-                <h3>Comment</h3>
+        <div class="card-item">
+            <div class="image-container">
+                <img src="/card.png" alt="card">
             </div>
-            <div class="like">
-                <img src="/like.svg" alt="like">
-                <h3>Like</h3>
-                <div class="share">
-                    <img src="/share.svg" alt="comment">
-                    <h3>Comment</h3>
+            <div class="content">
+                <div class="content-main">
+                    <h1>Ngobrolin Masalah Keluarga</h1>
+                    <p>lorem ipsum dskjfkldasjfkadsjfkljadsklfjkldsjfkldsjfkladsjfkladsjfkladsjkfljdasklfjkadsljffjkadsfdlkdjsfkladsklfkdsljflkdsjflkdasjfkldasjfkladsjfkl;adsmc,nzcxmnvjhgounfkldsnjadshfioadsjfkljadsl;fjadskljflkadsfkldsajfkladsjfkladslkfhewiofhljadsnfjlsdnm,ncm,xn lcioSF;</p>
+                </div>
+                <div class="content-buttons">
+                    <div class="content-buttons-group">
+                        <img src="/comment.svg" alt="comment">
+                        <p>Comment</p>
+                    </div>
+                    <div class="content-buttons-group">
+                        <img src="/like.svg" alt="like">
+                        <p>Like</p>
+                    </div>
+                    <div class="content-buttons-group">
+                        <img src="/share.svg" alt="share">
+                        <p>Share</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -74,6 +89,7 @@
     main{
         padding-left: 16rem;
         padding-top: 3rem;
+        color: white;
     }
 
     main .search{
@@ -94,28 +110,59 @@
         margin: 14px;
     }
 
-    main .cari{
-        padding-top: 2rem;
-        
+    .card-container {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        width: 90%;
+        margin-top: 32px;
     }
 
-    main .cari .card{
+    .card-item {
+        border-radius: 16px;
+        background-color: #2E2B2B;
+        padding: 16px;
+        width: 100%;
         display: flex;
-        align-items: center;
-        justify-content: start;
-        padding: 0.5rem;
-        border: 4px solid;
-        color: white;
-        width: 910px;
-        height: 231px;
+        gap: 16px;
     }
 
-    main .cari h1{
-        font-size: 24px;
+    .image-container {
+        display: flex;
+        gap: 8px;
+    }
+
+    .content {
+        flex-grow: 1;
+        width: calc(100% - 300px);
+        position: relative;
+    }
+
+    .content-main {
+        width: 100%;
+        height: 100%;
+    }
+
+    .content-main p {
+        word-wrap: break-word;
+        font-size: 1.15em;
+    }
+
+    .content-buttons {
+        display: flex;
+        gap: 32px;
+        align-items: center;
+        position: absolute;
+        bottom: 0;
+    }
+
+    .content-buttons-group {
         display: flex;
         align-items: center;
-        justify-content: start;
+        gap: 8px;
+        cursor: pointer;
     }
+
 
 
   
